87e04ba61b8f41f7afc7aba1f3e9b9233c8e8cdf
mozammil05 <mozammil.raja@antiersolutions.com>
2023-10-25 17:25:29 +0530

    fix db env

--- utils/database.go
+++ utils/database.go
@@ -170,23 +170,95 @@
 // 	}
 // }
 
+// package utils
+
+// import (
+// 	"context"
+
+// 	"go.mongodb.org/mongo-driver/mongo"
+// 	"go.mongodb.org/mongo-driver/mongo/options"
+// )
+
+// var (
+// 	mongoURI          = "mongodb://localhost:27017"
+// 	databaseName      = "yourapp"
+// 	userCollection    = "users"
+// 	productCollection = "products" // Add your collection names
+// 	// Add more collection names as needed
+// )
+
+// type Database struct {
+// 	Client            *mongo.Client
+// 	UserCollection    *mongo.Collection
+// 	ProductCollection *mongo.Collection // Add more collection fields as needed
+// 	// Add additional collection fields here
+// }
+
+// func InitDB() *Database {
+// 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	ctx := context.TODO()
+
+// 	err = client.Connect(ctx)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	db := &Database{
+// 		Client:            client,
+// 		UserCollection:    client.Database(databaseName).Collection(userCollection),
+// 		ProductCollection: client.Database(databaseName).Collection(productCollection),
+// 		// Assign more collections as needed
+// 	}
+
+// 	return db
+// }
+
+// func (db *Database) Disconnect() {
+// 	err := db.Client.Disconnect(context.TODO())
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
 package utils
 
 import (
 	"context"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// Define package-level variables here
 var (
-	mongoURI          = "mongodb://localhost:27017"
-	databaseName      = "yourapp"
+	// MongoDB connection string
+	mongoURI = getEnv("MONGO_URI", "mongodb://localhost:27017")
+
+	// Database name
+	databaseName = getEnv("DATABASE_NAME", "yourapp")
+
+	// User collection
+	// userCollection    = getEnv("USER_COLLECTION", "users")
+	// productCollection = getEnv("PRODUCT_COLLECTION", "products")
 	userCollection    = "users"
-	productCollection = "products" // Add your collection names
-	// Add more collection names as needed
+	productCollection = "products"
 )
 
+// getEnv is a helper function to read an environment variable or provide a default value.
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
+
+// Database represents the MongoDB database and collections.
 type Database struct {
 	Client            *mongo.Client
 	UserCollection    *mongo.Collection
@@ -194,6 +266,7 @@ type Database struct {
 	// Add additional collection fields here
 }
 
+// InitDB initializes the database connection.
 func InitDB() *Database {
 	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 	if err != nil {
@@ -217,6 +290,7 @@ func InitDB() *Database {
 	return db
 }
 
+// Disconnect closes the database connection.
 func (db *Database) Disconnect() {
 	err := db.Client.Disconnect(context.TODO())
 	if err != nil {
